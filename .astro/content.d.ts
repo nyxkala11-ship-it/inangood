9f9ad8d744206dccb7b88774d811877c89ef9e49
nyxkala11-ship-it <nyx.kala11@gmail.com>
2026-05-12 14:12:17 +0700

    chinh sua sdt xem truoc mau demo

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -184,6 +184,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = typeof import("../src/content.config.js");
+	export type ContentConfig = typeof import("./../src/content.config.js");
 	export type LiveContentConfig = never;
 }
