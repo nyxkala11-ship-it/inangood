2731c9213ef44ed2f90d83a7277a1de9b70f1bba
nyxkala11-ship-it <nyx.kala11@gmail.com>
2026-05-27 11:03:02 +0700

    Hoàn thiện chức năng tìm kiếm và thẻ sản phẩm (optional? chưa thêm rss) - add Adapter cho vercel deploy hybrid server side

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
